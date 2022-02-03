8b86c6ee2beffa480215ba373af2ba2107033680
Kyriakos-Bekas <kyriakosbekas00@gmail.com>
2022-02-03 02:39:23 +0200

    First commit

---
+++ components/Categories.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { getCategories } from '../services'
+
+const Categories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, [])
+
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
+      {categories.map((category) => (
+        <Link
+          href={`/category/${category.slug}`}
+          key={category.slug}
+          className="text-md"
+        >
+          <span className="mb-3 block cursor-pointer pb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Categories
---
+++ components/Comments.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+import { getComments } from '../services'
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result))
+  }, [])
+
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+            {comments.length}
+            {` `}
+            {comments.length == 1 ? 'Comment' : 'Comments'}
+          </h3>
+          {comments.map((comment) => (
+            <div
+              className="boder-gray-100 mb-4 border-b pb-4"
+              key={comment.createdAt}
+            >
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span>
+                {` `}
+                on
+                {` `}
+                {moment(comment.createdAt).format('MMM DD, YYYY')}
+              </p>
+              <p className="w-full whitespace-pre-line text-gray-600">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Comments
